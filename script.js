58121ef777b614bd86a504dca06d6b119773c5b4
Adelabu Mobolaji <142588160+Bolazcoding@users.noreply.github.com>
2025-04-28 20:32:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-"use strict";
+'use strict';
 
-const billInput = document.querySelector("#bill-input");
-const billNumber = document.querySelector("#bill-number");
-const tipBtn = document.querySelectorAll(".tip-btn");
-const Btn = document.querySelectorAll("button");
+const billInput = document.querySelector('#bill-input');
+const billNumber = document.querySelector('#bill-number');
+const tipBtn = document.querySelectorAll('.tip-btn');
+const Btn = document.querySelectorAll('button');
 
-const btnCustom = document.querySelector(".btn-custom");
-const btnActive = document.querySelector(".btn-active");
+const btnCustom = document.querySelector('.btn-custom');
+const btnActive = document.querySelector('.btn-active');
 
-const amountPerson = document.querySelector(".amountPerPerson");
-const tipTotal = document.querySelector(".tip-total");
+const amountPerson = document.querySelector('.amountPerPerson');
+const tipTotal = document.querySelector('.tip-total');
 
-const numZero = document.querySelector(".numZero");
-const reset = document.querySelector(".reset");
+const numZero = document.querySelector('.numZero');
+const reset = document.querySelector('.reset');
 
 let bill, percentage, number;
 let tipAmount, totalAmount, amountPerPerson;
 
-billInput.addEventListener("input", function () {
+billInput.addEventListener('input', function () {
   //   console.log(this.value);
   bill = Number(this.value);
   calculateTip();
 });
 
-billNumber.addEventListener("input", function () {
+billNumber.addEventListener('input', function () {
   //   console.log(this.value);
   number = Number(this.value);
   calculateTip();
 });
 
-btnCustom.addEventListener("input", function () {
+btnCustom.addEventListener('input', function () {
   //   console.log(this.value);
   percentage = Number(this.value);
   calculateTip();
 });
 
 for (let i = 0; i < tipBtn.length; i++) {
-  tipBtn[i].addEventListener("click", function () {
+  tipBtn[i].addEventListener('click', function () {
     // console.log(this.value);
     percentage = Number(this.value);
-    tipBtn[i].classList.toggle("btn-active");
+    // tipBtn[i].classList.toggle("btn-active");
+    tipBtn.forEach(function (e) {
+      e.classList.remove('btn-active');
+    });
+    this.classList.add('btn-active');
 
     calculateTip();
   });
@@ -50,8 +54,8 @@ function calculateTip(
   tipPercentage = percentage,
   numPeople = number
 ) {
-  if (numPeople === 0) {
-    numZero.classList.remove("numZ");
+  if (numPeople == 0) {
+    numZero.classList.remove('numZ');
     // numZero.textContent = "Can't be zero";
   } else {
     tipAmount = (tipPercentage / 100) * billAmount;
@@ -70,26 +74,30 @@ function calculateTip(
     amountPerson.textContent = `$${formattedtipAmount.toFixed(2)}`;
     tipTotal.textContent = `$${amountPerPerson.toFixed(2)}`;
 
-    numZero.classList.add("numZ");
+    numZero.classList.add('numZ');
     // numZero.textContent = "Can't be empty";
   }
 }
 
-reset.addEventListener("click", function () {
-  bill = percentage = number = " ";
+reset.addEventListener('click', function () {
+  bill = percentage = number = ' ';
   billInput.value = null;
-  billInput.placeholder = "0";
+  billInput.placeholder = '0';
 
   billNumber.value = null;
-  billNumber.placehoslder = "0";
+  billNumber.placehoslder = '0';
 
   btnCustom.value = null;
-  btnCustom.placeholder = "Custom";
+  btnCustom.placeholder = 'Custom';
 
-  tipAmount = totalAmount = amountPerPerson = " ";
+  tipAmount = totalAmount = amountPerPerson = ' ';
 
-  amountPerson.textContent = "$0.00";
-  tipTotal.textContent = "$0.00";
+  amountPerson.textContent = '$0.00';
+  tipTotal.textContent = '$0.00';
 
-  numZero.classList.add("numZ");
+  numZero.classList.add('numZ');
+
+  tipBtn.forEach(function (e) {
+    e.classList.remove('btn-active');
+  });
 });
